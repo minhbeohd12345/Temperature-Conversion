512698f74ab06c40fef682b35e8abb9178ec6d97
Phasewalker <135408665+minhbeohd12345@users.noreply.github.com>
2025-09-03 14:56:31 +0700

    Update Temp.js

--- Temp.js
+++ Temp.js
@@ -1,10 +1,10 @@
 console.log("Temperature Conversion")
 function Celsius_To_Fahrenheit(Celsius){
   let Result = (Celsius * 9/5) + 32;
-document.getElementById("demo").innerHTML = Result
+document.getElementById("demo").innerHTML = Result.toFixed(3) + "*F";
 }
 
 function Fahrenheit_To_Celsius(Fahrenheit){
   let Result2 = (Fahrenheit - 32) * 5/9;
-document.getElementById("demo2").innerHTML = Result2
+document.getElementById("demo2").innerHTML = Result2.toFixed(3) + "*C";
 }
